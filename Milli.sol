2e11b200cbd65bd18b6439b1dc5f7b684706b174
unknown <mandocustom@gmail.com>
2021-08-25 10:01:40 +0900

    first commit

---
+++ Milli.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.19;
+
+contract Kata {
+         function past(int h, int m, int i) public pure returns (_seconds){
+            _seconds = (   )
+             return _seconds
+         }
+         function hoursToSeconds() public pure (_hours){
+             _hours = _hours * 3600; 
+         }
+         function minutestoSeconds(_) public pure (_minutes){
+             _minutes = _minutes * 60;
+         }
+         function secondsToMilli(_) public pure (_milli){
+             _milli = _seconds * 1000
+
+         }
+
+
+}
